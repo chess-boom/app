368b2136f975cf8ccf2ebb2bd3d8586778ceb883
btakli <64867481+btakli@users.noreply.github.com>
2022-10-16 21:20:47 -0400

    Fixed linting issue

--- src/ViewLocator.cs
+++ src/ViewLocator.cs
@@ -16,7 +16,7 @@ namespace ChessBoom
             {
                 return (Control)Activator.CreateInstance(type)!;
             }
-            
+
             return new TextBlock { Text = "Not Found: " + name };
         }
 
