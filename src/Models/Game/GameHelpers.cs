f3ddfe8c86f7b7ce5049e20cbbb9716d332c97d2
MatteoGisondi <matteo.gisondi@gmail.com>
2022-11-08 01:23:24 -0500

    refactor: :recycle: Make readonly
    [SonarQube issue](https://sonar.chessboom.net/project/issues?issues=AYRLfI10lgjAxyDhVx1t&open=AYRLfI10lgjAxyDhVx1t&id=Chess-Boom-App)

--- src/Models/Game/GameHelpers.cs
+++ src/Models/Game/GameHelpers.cs
@@ -11,20 +11,20 @@ namespace ChessBoom.Models.Game
         /// <summary>
         /// The names of the columns of the chess board are here defined
         /// </summary>
-        public static string[] k_BoardColumnNames = { "a", "b", "c", "d", "e", "f", "g", "h" };
+        public static readonly string[] k_BoardColumnNames = { "a", "b", "c", "d", "e", "f", "g", "h" };
         /// <summary>
         /// The names of the rows of the chess board are here defined
         /// </summary>
-        public static string[] k_BoardRowNames = { "1", "2", "3", "4", "5", "6", "7", "8" };
+        public static readonly string[] k_BoardRowNames = { "1", "2", "3", "4", "5", "6", "7", "8" };
 
         /// <summary>
         /// The width of the chess board is here defined
         /// </summary>
-        public static int k_BoardWidth = 8;
+        public static readonly int k_BoardWidth = 8;
         /// <summary>
         /// The height of the chess board is here defined
         /// </summary>
-        public static int k_BoardHeight = 8;
+        public static readonly int k_BoardHeight = 8;
 
         /// <summary>
         /// The corresponding square is retrieved from its board coordinates
