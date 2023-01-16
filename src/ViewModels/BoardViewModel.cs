c77d6d3f87fbd79476f8db111393c19d4484603e
MatteoGisondi <matteo.gisondi@gmail.com>
2023-01-16 13:50:40 -0500

    feat(CB-89): :construction: Adding dynamic pieces

--- src/ViewModels/BoardViewModel.cs
+++ src/ViewModels/BoardViewModel.cs
@@ -9,23 +9,9 @@ namespace ChessBoom.ViewModels;
 public class BoardViewModel : BaseViewModel
 {
     public Game game;
-    public List<Piece> pieces { get; }
-    public string c => "p";
 
     public BoardViewModel(IScreen hostScreen) : base(hostScreen)
     {
         game = new Game();
-        pieces = game.m_board.m_pieces;
     }
-
-    public string GetString(Piece p)
-    {
-        return p.ToString() ?? throw new InvalidOperationException();
-    }
-
-    public List<Piece> GetPieces()
-    {
-        return pieces;
-    }
-
 }
--- src/Views/BoardView.axaml.cs
+++ src/Views/BoardView.axaml.cs
@@ -1,53 +1,94 @@
-using Avalonia.Markup.Xaml;
-using Avalonia.ReactiveUI;
-using ChessBoom.ViewModels;
-using Avalonia.Svg.Skia;
-using ReactiveUI;
-using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 using Avalonia.Media;
-using Avalonia;
+using Avalonia.ReactiveUI;
+using ChessBoom.Models.Game.Pieces;
+using ChessBoom.Models.Game;
+using ChessBoom.ViewModels;
+using ReactiveUI;
+using SkiaSharp;
+using Svg.Skia;
 
 namespace ChessBoom.Views;
 
 public partial class BoardView : ReactiveUserControl<BoardViewModel>
 {
-    //private Grid chessBoard;
+    private abstract class Tile
+    {
+        internal static int Width => 50;
+        internal static int Height => 50;
+
+        internal static readonly Color WHITE = Color.FromRgb(243, 219, 180);
+        internal static readonly Color BLACK = Color.FromRgb(179, 140, 99);
+    }
+
     public BoardView()
     {
-        this.WhenActivated(_ => { });
+        ChessBoard = this.Find<Grid>("ChessBoard");
+        this.WhenActivated(_ =>
+        {
+            DrawChessBoard();
+            DrawPieces();
+        });
         AvaloniaXamlLoader.Load(this);
-        chessBoard = this.Find<Grid>("chessBoard");
-        DrawChessBoard();
     }
 
     private void DrawChessBoard()
+    {
+        for (var i = 0; i < GameHelpers.k_boardWidth; i++)
         {
-            for (int i = 0; i < 8; i++)
+            for (var j = 0; j < GameHelpers.k_boardHeight; j++)
             {
-                for (int j = 0; j < 8; j++)
+                var tile = new Rectangle
                 {
-                    var rect = new Rectangle();
-                    rect.Width = 50;
-                    rect.Height = 50;
-                    rect.StrokeThickness = 0;
-                    if ((i + j) % 2 == 0)
-                    {
-                        rect.Fill = new SolidColorBrush(Color.FromRgb(243,219,180));
-                    }
-                    else
-                    {
-                        rect.Fill = new SolidColorBrush(Color.FromRgb(179,140,99));
-                    }
-                    Grid.SetRow(rect, i);
-                    Grid.SetColumn(rect, j);
-                    chessBoard.Children.Add(rect);
-                }
-            }
-            for (int i = 0; i < 8; i++)
-            {
-                chessBoard.RowDefinitions.Add(new RowDefinition());
-                chessBoard.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(49, GridUnitType.Pixel) });
+                    Width = Tile.Width,
+                    Height = Tile.Height,
+                    Name = GameHelpers.GetSquareFromCoordinate((i, j)),
+                    StrokeThickness = 0,
+                    Fill = (i + j) % 2 == 0 ? new SolidColorBrush(Tile.WHITE) : new SolidColorBrush(Tile.BLACK)
+                };
+                Grid.SetRow(tile, i);
+                Grid.SetColumn(tile, j);
+                ChessBoard.Children.Add(tile);
             }
         }
+
+        for (var i = 0; i < GameHelpers.k_boardWidth; i++)
+        {
+            ChessBoard.RowDefinitions.Add(new RowDefinition());
+            ChessBoard.ColumnDefinitions.Add(new ColumnDefinition
+                {
+                    Width = new GridLength(Tile.Width, GridUnitType.Pixel)
+                }
+            );
+        }
+    }
+
+    private void DrawPieces()
+    {
+        // In Progress
+        // using (var bitmap = new SKBitmap(width, height))
+        // using (var canvas = new SKCanvas(bitmap))
+        // {
+        //     var piece = new Pawn(ViewModel.game.m_board, Player.White, (0, 0));
+        //     var PiecePath = piece.GetPlayer() == Player.White
+        //         ? $"Assets/Pieces/{piece}.svg"
+        //         : $"Assets/Pieces/{piece}_.svg";
+        //     var svg = new SKSvg();
+        //     if (svg.Load(PiecePath) is { })
+        //     {
+        //         var svgControl = new SvgControl();
+        //         svgControl.Svg = svg;
+        //     }
+
+        //     Svg.Draw(canvas, svg);
+        //     var image = SKImage.FromBitmap(bitmap);
+        // }
+        // foreach (var piece in ViewModel.game.m_board.m_pieces)
+        // {
+        //     var tile = this.Find<Rectangle>(GameHelpers.GetSquareFromCoordinate(piece.GetCoordinates()));
+        //     var
+        // }
+    }
 }
