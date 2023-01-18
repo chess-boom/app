eb5cae78462296fcf1dd43195032dc626848c53d
MatteoGisondi <matteo.gisondi@gmail.com>
2023-01-17 22:48:37 -0500

    feat(CB-89): :construction: Add Svg on board

--- src/Views/BoardView.axaml.cs
+++ src/Views/BoardView.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls.Shapes;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -19,72 +20,69 @@ public partial class BoardView : ReactiveUserControl<BoardViewModel>
         internal static int Width => 50;
         internal static int Height => 50;
 
-        internal static readonly Color WHITE = Color.FromRgb(243, 219, 180);
-        internal static readonly Color BLACK = Color.FromRgb(179, 140, 99);
+        internal static readonly Color White = Color.FromRgb(243, 219, 180);
+        internal static readonly Color Black = Color.FromRgb(179, 140, 99);
+    }
+
+    private abstract class Piece
+    {
+        internal const string White = "Assets/Pieces/{0}.svg";
+        internal const string Black = "Assets/Pieces/{0}_.svg";
     }
 
     public BoardView()
     {
-        ChessBoard = this.Find<Grid>("ChessBoard");
         this.WhenActivated(_ =>
         {
             DrawChessBoard();
             DrawPieces();
         });
         AvaloniaXamlLoader.Load(this);
+        ChessBoard = this.Find<Grid>("ChessBoard");
     }
 
     private void DrawChessBoard()
     {
         for (var i = 0; i < GameHelpers.k_boardWidth; i++)
         {
+            ChessBoard.ColumnDefinitions.Add(new ColumnDefinition());
             for (var j = 0; j < GameHelpers.k_boardHeight; j++)
             {
+                ChessBoard.RowDefinitions.Add(new RowDefinition());
                 var tile = new Rectangle
                 {
                     Width = Tile.Width,
                     Height = Tile.Height,
                     Name = GameHelpers.GetSquareFromCoordinate((i, j)),
                     StrokeThickness = 0,
-                    Fill = (i + j) % 2 == 0 ? new SolidColorBrush(Tile.WHITE) : new SolidColorBrush(Tile.BLACK)
+                    Fill = (i + j) % 2 == 0 ? new SolidColorBrush(Tile.White) : new SolidColorBrush(Tile.Black)
                 };
                 Grid.SetRow(tile, i);
                 Grid.SetColumn(tile, j);
                 ChessBoard.Children.Add(tile);
             }
         }
-
-        for (var i = 0; i < GameHelpers.k_boardWidth; i++)
-        {
-            ChessBoard.RowDefinitions.Add(new RowDefinition());
-            ChessBoard.ColumnDefinitions.Add(new ColumnDefinition
-                {
-                    Width = new GridLength(Tile.Width, GridUnitType.Pixel)
-                }
-            );
-        }
     }
 
     private void DrawPieces()
     {
         // In Progress
-        // using (var bitmap = new SKBitmap(width, height))
-        // using (var canvas = new SKCanvas(bitmap))
+        // using var bitmap = new SKBitmap(Tile.Width, Tile.Height);
+        // using var canvas = new SKCanvas(bitmap);
+        // var piece = new Pawn(ViewModel!.game.m_board, Player.Black, (0, 0));
+        // var piecePath = piece.GetPlayer() switch
         // {
-        //     var piece = new Pawn(ViewModel.game.m_board, Player.White, (0, 0));
-        //     var PiecePath = piece.GetPlayer() == Player.White
-        //         ? $"Assets/Pieces/{piece}.svg"
-        //         : $"Assets/Pieces/{piece}_.svg";
-        //     var svg = new SKSvg();
-        //     if (svg.Load(PiecePath) is { })
-        //     {
-        //         var svgControl = new SvgControl();
-        //         svgControl.Svg = svg;
-        //     }
-
-        //     Svg.Draw(canvas, svg);
-        //     var image = SKImage.FromBitmap(bitmap);
-        // }
+        //     Player.White => string.Format(Piece.White, piece),
+        //     Player.Black => string.Format(Piece.Black, piece),
+        //     _ => throw new ArgumentOutOfRangeException()
+        // };
+        // var svg = new SKSvg();
+        // svg.Load(piecePath);
+        // canvas.DrawPicture(svg.Picture);
+        
+        // Grid.SetRow(canvas, 0);
+        // Grid.SetColumn(canvas, 0);
+        
         // foreach (var piece in ViewModel.game.m_board.m_pieces)
         // {
         //     var tile = this.Find<Rectangle>(GameHelpers.GetSquareFromCoordinate(piece.GetCoordinates()));
