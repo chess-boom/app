c9bf116a44d9cefa8c5dc1124e68d1a6a2e60bdb
btakli <64867481+btakli@users.noreply.github.com>
2023-02-10 02:01:43 -0500

    refactor(CB-112): :art: Made GetNBestMoves return an empty list instead of null in the event that we cannot get an evaluation

--- src/Models/Analysis/Stockfish.cs
+++ src/Models/Analysis/Stockfish.cs
@@ -215,7 +215,7 @@ public class Stockfish : IAnalysis
     /// <param name="n">Number of moves you want to return. Value greater than 0. Ordered by descending cp value). Default: 3</param>
     /// <param name="depth">Depth you want the search to go to. Value greater than 0. Default: 10</param>
     /// <returns>List of (string, int) tuples, representing (move, cp value). Ordered from highest cp to lowest cp value moves</returns>
-    public List<(string, int)>? GetNBestMoves(int n = 3, int depth = 10)
+    public List<(string, int)> GetNBestMoves(int n = 3, int depth = 10)
     {
         if (n <= 0 || depth <= 0)
             throw new ArgumentOutOfRangeException("Depth and n must be greater than 0.");
@@ -236,7 +236,7 @@ public class Stockfish : IAnalysis
 
             if (output is null)
             {
-                return null;
+                return new List<(string, int)>();
             }
 
             outputList.Add(output);
@@ -278,7 +278,7 @@ public class Stockfish : IAnalysis
         }
         else
         {
-            return null;
+            return new List<(string, int)>();
         }
 
 
