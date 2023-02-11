bd2c2a0f5bf9797d890bf7a816d406f3de9ebe19
btakli <64867481+btakli@users.noreply.github.com>
2023-02-11 16:24:48 -0500

    refactor(CB-117): :memo: Addded comment to the AreEqual assertions of the Unit test AnalysisEngineReturnsStaticEvaluationForFen

--- ChessBoom.NUnitTests/AnalysisTests/AnalysisUnitTests.cs
+++ ChessBoom.NUnitTests/AnalysisTests/AnalysisUnitTests.cs
@@ -41,11 +41,11 @@ public class AnalysisUnitTests
             // The mac version is more recent and uses a different evaluation function, so the values are different.
             if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                Assert.AreEqual(-0.18f, staticEval.FinalEvaluation);
+                Assert.AreEqual(-0.18f, staticEval.FinalEvaluation, "This value may change if we update Stockfish"); 
             }
             else
             {
-                Assert.AreEqual(-0.07f, staticEval.FinalEvaluation);
+                Assert.AreEqual(-0.07f, staticEval.FinalEvaluation, "This value may change if we update Stockfish");
             }
 
             Assert.AreEqual('w', staticEval.Side);
