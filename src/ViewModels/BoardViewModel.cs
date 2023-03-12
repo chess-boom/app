69f27c7da68b1429b677da01d4129f1b37f0ad9d
MatteoGisondi <matteo.gisondi@gmail.com>
2023-03-12 03:49:36 -0400

    style(CB-123): :rotating_light: ViewModels/BoardViewModel.cs(13,62): Fix whitespace formatting

--- src/ViewModels/BoardViewModel.cs
+++ src/ViewModels/BoardViewModel.cs
@@ -10,7 +10,7 @@ public class BoardViewModel : BaseViewModel
     public GameHandler GameHandler { get; }
     public string Title { get; }
 
-    public BoardViewModel(IScreen hostScreen, Variant variant=Variant.Standard) : base(hostScreen)
+    public BoardViewModel(IScreen hostScreen, Variant variant = Variant.Standard) : base(hostScreen)
     {
         Title = variant == Variant.Standard ? "Board" : $"Board: {variant}";
         GameHandler = new GameHandler(variant);
