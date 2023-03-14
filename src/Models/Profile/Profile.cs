ae151c0b7871e60d4d7ce1c000c52b19a6e4728e
andrei-grosuliac <agrosuliac@gmail.com>
2023-03-14 14:23:09 -0400

    feat: :sparkles: eloGraph
    incompatibility of system.windows.media with dotnet version

--- src/Models/Profile/Profile.cs
+++ src/Models/Profile/Profile.cs
@@ -1,25 +1,65 @@
 using System;
 using System.Collections.Generic;
-using ReactiveUI;
+using System.Drawing;
 
 namespace ChessBoom.Models.Profile;
 
 public class Profile{
-    public BarGraph barGraph { get; set; }
     public string name { get; set; }
-    public List<int> elo { get; set; }
-    public double winRateWhite { get; set; }
-    public double winRateBlack { get; set; }
-    public double lossRateWhite { get; set; }
-    public double lossRateBlack { get; set; }
-    public double drawRateWhite { get; set; }
-    public double drawRateBlack { get; set; }
-    public List<int> barGraphData { get; set; }
+    public string[][] elo { get; set; } //elo, difference, pixels, date
+    public readonly int lastEloGames = 10;
+    public Queue<string[]> eloQueue { get; set; }
+    public int graphMaxElo { get; set; }
+    public int graphMinElo { get; set; }
+    //public PointCollection Points {get; set;}
+    public System.Windows.Point graphStartTrend { get; set; }
+    public Point graphEndTrend { get; set; }
+    double _winRateWhite;
+    public int winRateWhite {
+        get{return (int)Math.Ceiling(_winRateWhite*100);} 
+        set{_winRateWhite = value;} 
+    }
+    double _winRateBlack;
+    public int winRateBlack { 
+        get{return (int)Math.Ceiling(_winRateBlack*100);} 
+        set{_winRateBlack = value;} 
+    }
+    double _lossRateWhite;
+    public int lossRateWhite { 
+        get{return (int)Math.Ceiling(_lossRateWhite*100);} 
+        set{_lossRateWhite = value;} 
+    }
+    double _lossRateBlack;
+    public int lossRateBlack { 
+        get{return (int)Math.Ceiling(_lossRateBlack*100);} 
+        set{_lossRateBlack = value;} 
+    }
+    double _drawRateWhite;
+    public int drawRateWhite { 
+        get{return (int)Math.Ceiling(_drawRateWhite*100);} 
+        set{_drawRateWhite = value;} 
+    }
+    double _drawRateBlack;
+    public int drawRateBlack { 
+        get{return (int)Math.Ceiling(_drawRateBlack*100);} 
+        set{_drawRateBlack = value;} 
+    }
+    public int[] barGraphData { get; set; }
     public List<Dictionary<string, string>> games { get; set; }
     public int totalGames { get; set; }
+    public int wins { get; set; }
+    public int losses { get; set; }
+    public int draws { get; set; }
+    public int[] whiteBars { get; set; }
+    public int[] blackBars { get; set; }
+    public readonly int barWidth = 600;
+    public readonly int barHeight = 200;
+    public readonly int eloGraphHeight = 400;
     public Profile(){
         this.name = "Default";
-        this.elo = new List<int>();
+        this.elo = new string[this.lastEloGames][];
+        this.eloQueue = new Queue<string[]>();
+        //this.Points = new PointCollection();
         this.winRateWhite = 0;
         this.winRateBlack = 0;
         this.lossRateWhite = 0;
@@ -28,22 +68,26 @@ public class Profile{
         this.drawRateBlack = 0;
         this.totalGames = 0;
         this.games = new List<Dictionary<string, string>>();
-        this.barGraphData = new List<int>();
-        this.barGraph = new BarGraph();
+        this.barGraphData = new int[3];
+        this.whiteBars = new int[3];
+        this.blackBars = new int[3];
     }
     public Profile(string name){
         this.name = name;
-        this.elo = new List<int>();
+        this.elo = new string[this.lastEloGames][];
+        this.eloQueue = new Queue<string[]>();
+        //this.Points = new PointCollection();
         this.winRateWhite = 0;
         this.winRateBlack = 0;
         this.lossRateWhite = 0;
         this.lossRateBlack = 0;
         this.drawRateWhite = 0;
         this.drawRateBlack = 0;
-        this.totalGames = 110;
+        this.totalGames = 0;
         this.games = new List<Dictionary<string, string>>();
-        this.barGraphData = new List<int>();
-        this.barGraph = new BarGraph();
+        this.barGraphData = new int[3];
+        this.whiteBars = new int[3];
+        this.blackBars = new int[3];
     }
 
     public void addGame(Dictionary<string, string> game){
@@ -77,7 +121,7 @@ public class Profile{
                 } else {
                     whiteDraws++;
                 }
-                addElo(int.Parse(game["WhiteElo"]));
+                addElo(game["WhiteElo"], game["Date"]);
                 whiteGames++;
             } else {
                 if (game["Result"] == "1-0"){
@@ -87,41 +131,133 @@ public class Profile{
                 } else {
                     blackDraws++;
                 }
-                addElo(int.Parse(game["BlackElo"]));
+                addElo(game["BlackElo"], game["Date"]);
                 blackGames++;
             }
         }
-        //solves division by 0
+        // solves division by 0
         if(whiteGames == 0) whiteGames = 1;
         if(blackGames == 0) blackGames = 1;
+        if(totalGames == 0) totalGames = 1;
 
-        winRateWhite = Math.Round((double)whiteWins / whiteGames, 2);
-        winRateBlack = Math.Round((double)blackWins / blackGames, 2);
-        lossRateWhite = Math.Round((double)whiteLosses / whiteGames, 2);
-        lossRateBlack = Math.Round((double)blackLosses / blackGames, 2);
-        drawRateWhite = Math.Round((double)whiteDraws / whiteGames, 2);
-        drawRateBlack = Math.Round((double)blackDraws / blackGames, 2);
-        wins = whiteWins + blackWins;
-        losses = whiteLosses + blackLosses;
-        draws = whiteDraws + blackDraws;
+        // Total Games
         this.totalGames = totalGames;
+
+        // Results %
+        _winRateWhite = Math.Round((double)whiteWins / whiteGames, 2);
+        _winRateBlack = Math.Round((double)blackWins / blackGames, 2);
+        _lossRateWhite = Math.Round((double)whiteLosses / whiteGames, 2);
+        _lossRateBlack = Math.Round((double)blackLosses / blackGames, 2);
+        _drawRateWhite = Math.Round((double)whiteDraws / whiteGames, 2);
+        _drawRateBlack = Math.Round((double)blackDraws / blackGames, 2);
+
+        // Total Games
+        this.totalGames = totalGames;
+
+        // Black/White Win Ratio Bars
+        whiteBars[0] = (int)Math.Ceiling(_winRateWhite * barWidth);
+        whiteBars[1] = (int)Math.Ceiling(_lossRateWhite * barWidth);
+        whiteBars[2] = barWidth - whiteBars[0] - whiteBars[1];
+
+        blackBars[0] = (int)Math.Ceiling(_winRateBlack * barWidth);
+        blackBars[1] = (int)Math.Ceiling(_lossRateBlack * barWidth);
+        blackBars[2] = barWidth - blackBars[0] - blackBars[1];
+
+        // Results numbers
+        wins = whiteWins + blackWins;
+        this.wins = wins;
+        losses = whiteLosses + blackLosses;
+        this.losses = losses;
+        draws = whiteDraws + blackDraws;
+        this.draws = draws;
+
+        // BarGraph Bars
+        barGraphData[0] = (int)(((double)wins / totalGames) * barHeight);
+        barGraphData[1] = (int)(((double)losses / totalGames) * barHeight);
+        barGraphData[2] = barHeight - barGraphData[0] - barGraphData[1];
+
+        // Elo Graph
+        transformQueueToArray();
+        calculateTrendLine();
+        Console.WriteLine(this.graphStartTrend);
+        Console.WriteLine(this.graphEndTrend);
     }
 
-    public void addElo(int elo){
-        this.elo.Add(elo);
+    public void calculateTrendLine(){
+        var yAxisValues = new List<int>();
+        var xAxisValues = new List<int>();
+
+        for (int i = 0; i < this.lastEloGames; i++)
+        {
+            yAxisValues.Add(int.Parse(this.elo[i][0]));
+            xAxisValues.Add(i*50+50);
+        }
+
+        Trendline treadline = new Trendline(yAxisValues, xAxisValues);
+        this.graphStartTrend = new Point((50, convertEloToPixels(treadline.Start)));
+        this.graphEndTrend = "500," + convertEloToPixels(treadline.End).ToString();
+    }
+
+    public void addElo(string elo, string date){
+        if(this.eloQueue.Count == this.lastEloGames){
+            this.eloQueue.Dequeue();
+        }
+        string[] info = {elo, date};
+        this.eloQueue.Enqueue(info);
+    }
+
+    public void transformQueueToArray(){
+        int difference = 0;
+        string diff = "";
+        string[] info;
+        int currentElo = 0;
+        int lastElo = 0;
+        int max = 0;
+        int min = int.MaxValue;
+        int pixels = 0;
+
+        for(int i = 9; i>=0; i--){
+            info = eloQueue.Dequeue();
+            currentElo = int.Parse(info[0]);
+            if(currentElo > max) max = currentElo;
+            if(currentElo < min) min = currentElo;            
+            difference = (i==9) ? 0 : (currentElo - lastElo);
+            diff = (difference > 0) ? "+" + difference.ToString() : difference.ToString();            
+            lastElo = currentElo;
+            string[] eloInfo = {info[0], diff, "", info[1]};
+            this.elo[i] = eloInfo;
+        }
+        this.graphMaxElo = (max+150) / 100 * 100;
+        this.graphMinElo = (min-150) / 100 * 100;
+        if(this.graphMaxElo < 0) this.graphMaxElo = 0;
+        if(this.graphMinElo < 0) this.graphMinElo = 0;
+
+        for(int i = 0; i<10; i++){
+            pixels = convertEloToPixels(int.Parse(elo[i][0]));
+            this.elo[i][2] = pixels.ToString();
+        }
+    }
+
+    public int convertEloToPixels(int elo){
+        double graphPosition = 0;
+        int pixels = 0;
+        int range = (this.graphMaxElo-this.graphMinElo);
+        graphPosition = ((double)(elo - this.graphMinElo)) / range;
+        pixels = eloGraphHeight - (int)(graphPosition * eloGraphHeight) + 30;
+        return pixels;
     }
 
     public string getEloHistory(){
         string history = "";
-        foreach (int e in elo){
-            history += "[" + e + "] ";
-        }
+        // foreach (string e in elo){
+        //     history += "[" + e + "] ";
+        // }
         return history;
     }
 
     public void displayProfileStats(){
         string stats = "Name: " + name + "\n";
-        stats += "Elo: " + getEloHistory() + "\n";
+        //stats += "Elo: " + getEloHistory() + "\n";
         stats += "Win Rate White: " + winRateWhite + "\n";
         stats += "Win Rate Black: " + winRateBlack + "\n";
         stats += "Loss Rate White: " + lossRateWhite + "\n";
