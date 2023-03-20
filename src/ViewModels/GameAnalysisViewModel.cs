80ab92c402ee3b1cd0f343c56c1efc34555dd597
MatteoGisondi <matteo.gisondi@gmail.com>
2023-03-20 12:22:27 -0400

    refactor(CB-127): :construction: WIP on Remove Game Analysis Code Smells

--- src/ViewModels/GameAnalysisViewModel.cs
+++ src/ViewModels/GameAnalysisViewModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using ReactiveUI;
 
@@ -6,7 +7,108 @@ namespace ChessBoom.ViewModels;
 [ExcludeFromCodeCoverage]
 public class GameAnalysisViewModel : BaseViewModel
 {
+    public class PlayerData
+    {
+        public string? Name { get; set; }
+        public string? Opponent { get; set; }
+        public string? Variant { get; set; }
+        public string? MatchStatus { get; set; }
+        public int? Moves { get; set; }
+        public int? Blunders { get; set; }
+        public int? MissedWins { get; set; }
+        public string? Improvement { get; set; }
+
+    }
+
+    private ObservableCollection<PlayerData> _playerDataList = new();
+
+    public ObservableCollection<PlayerData> PlayerDataList
+    {
+        get => _playerDataList;
+        set => this.RaiseAndSetIfChanged(ref _playerDataList, value);
+    }
+
     public GameAnalysisViewModel(IScreen hostScreen) : base(hostScreen)
     {
+        PlayerDataList = new ObservableCollection<PlayerData>
+        {
+            new()
+            {
+                Name = "J4N3D03",
+                Opponent = "J0ND03",
+                Variant = "Atomic Chess",
+                MatchStatus = "Win!",
+                Moves = 32,
+                Blunders = 2,
+                MissedWins = 0,
+                Improvement = "++"
+            },
+            new()
+            {
+                Name = "J4N3D03",
+                Opponent = "J0ND03",
+                Variant = "Atomic Chess",
+                MatchStatus = "Win!",
+                Moves = 32,
+                Blunders = 2,
+                MissedWins = 0,
+                Improvement = "++"
+            },
+            new()
+            {
+                Name = "J4N3D03",
+                Opponent = "J0ND03",
+                Variant = "Atomic Chess",
+                MatchStatus = "Win!",
+                Moves = 32,
+                Blunders = 2,
+                MissedWins = 0,
+                Improvement = "++"
+            },
+            new()
+            {
+                Name = "J4N3D03",
+                Opponent = "J0ND03",
+                Variant = "Atomic Chess",
+                MatchStatus = "Win!",
+                Moves = 32,
+                Blunders = 2,
+                MissedWins = 0,
+                Improvement = "++"
+            },
+            new()
+            {
+                Name = "J4N3D03",
+                Opponent = "J0ND03",
+                Variant = "Atomic Chess",
+                MatchStatus = "Win!",
+                Moves = 32,
+                Blunders = 2,
+                MissedWins = 0,
+                Improvement = "++"
+            },
+            new()
+            {
+                Name = "J4N3D03",
+                Opponent = "J0ND03",
+                Variant = "Atomic Chess",
+                MatchStatus = "Win!",
+                Moves = 32,
+                Blunders = 2,
+                MissedWins = 0,
+                Improvement = "++"
+            },
+            new()
+            {
+                Name = "J4N3D03",
+                Opponent = "J0ND03",
+                Variant = "Atomic Chess",
+                MatchStatus = "Win!",
+                Moves = 32,
+                Blunders = 2,
+                MissedWins = 0,
+                Improvement = "++"
+            }
+        };
     }
 }
--- src/Views/GameAnalysisView.axaml.cs
+++ src/Views/GameAnalysisView.axaml.cs
@@ -3,15 +3,30 @@ using Avalonia.ReactiveUI;
 using ChessBoom.ViewModels;
 using ReactiveUI;
 using System.Diagnostics.CodeAnalysis;
+using Avalonia.Controls;
 
 namespace ChessBoom.Views;
 
 [ExcludeFromCodeCoverage]
 public partial class GameAnalysisView : ReactiveUserControl<GameAnalysisViewModel>
 {
+    private DataGrid _playerData;
+    
     public GameAnalysisView()
     {
         this.WhenActivated(_ => { });
         AvaloniaXamlLoader.Load(this);
+        
+        _playerData = this.FindControl<DataGrid>("PlayerData");
+    }
+    
+    protected override void OnDataContextChanged(System.EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+
+        if (DataContext is not GameAnalysisViewModel viewModel) return;
+        
+        _playerData.DataContext = viewModel;
+        _playerData.Items = viewModel.PlayerDataList;
     }
 }
