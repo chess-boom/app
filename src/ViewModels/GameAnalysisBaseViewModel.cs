b5457117a56e48e53481fc68ae4611f33c50ecc9
Daniel Macicasan <danimacicasan@gmail.com>
2023-03-31 18:51:19 -0400

    feat: :sparkles: Added Select Game File button for the analysis

--- src/ViewModels/GameAnalysisBaseViewModel.cs
+++ src/ViewModels/GameAnalysisBaseViewModel.cs
@@ -1,7 +1,14 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Reactive;
+using Avalonia.Controls;
 using ChessBoom.Models.Game;
 using ReactiveUI;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ChessBoom.ViewModels;
 
@@ -14,10 +21,43 @@ public class GameAnalysisBaseViewModel : BoardViewModel
     public List<string> Pieces { get; set; }
     public List<string> LostPieces { get; set; }
     public List<string> CapturedPieces { get; set; }
+    public ReactiveCommand<Unit, Unit> OpenExplorerCommand { get; }
+    public string FileData { get; set; }
 
     public GameAnalysisBaseViewModel(IScreen hostScreen, Variant variant = Variant.Standard) : base(hostScreen)
     {
         Title = variant == Variant.Standard ? "Board" : $"Board: {variant}";
         GameHandler = new GameHandler(variant);
+        //FileData = "No File Selected";
+        OpenExplorerCommand = handleFileExplorer();
+    }
+
+    public static ReactiveCommand<Unit, Unit> handleFileExplorer()
+    {
+        return ReactiveCommand.CreateFromTask(async () =>
+        {
+            var parentWindow = new Window();
+            var dialog = new OpenFileDialog();
+            dialog.Title = "Select a file";
+            dialog.Directory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+            dialog.Filters.Add(new FileDialogFilter() { Name = "PGN Files", Extensions = { "pgn" } });
+            var result = await dialog.ShowAsync(parentWindow);
+            if (result != null)
+            {
+                var selectedFilePath = dialog.InitialFileName;
+                //FileData = selectedFilePath;
+                var file = result.FirstOrDefault();
+                if (file != null)
+                {
+                    // Read the file data
+                    //FileData = selectedFilePath;//await System.IO.File.ReadAllTextAsync(selectedFilePath);
+                }
+            }
+            else
+            {
+                // User clicked "Cancel" or closed the dialog
+                //FileData = "Canceled";
+            }
+        });
     }
 }
